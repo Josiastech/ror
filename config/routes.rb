5ab98d6b53f69f971d33e3750fc207990494ad67
Josias <jchocooj@vantagemedia.com>
2015-04-15 09:03:40 -0600

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
+  #get 'welcome/index'
+  resources :articles
+=begin
+  
+    get "/articles"
+    post "/articles"
+    delete "/articles"
+    get "/articles/:id"
+    get "/articles/new"
+    get "/articles/:id/edit"
+    patch "/articles/:id"
+    put "/articles/:id"
+=end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
